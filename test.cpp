fb499f263b5112ebd084e12638216eb3a0758cbf
lai81git <lai81.tom@gmail.com>
2019-05-10 17:10:08 +0800

    第二次新增

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+ 
+int main ()
+{
+ 
+   for(int i=1;i<10;i++)
+   {
+      printf("This loop will run forever.\n");
+   }
+   system("pause");
+   return 0;
+}
